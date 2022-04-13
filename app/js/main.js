b97fb3c0b74fa4adacb3d89056e2c426f294f5cf
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-04-13 10:52:59 +0300

    v1.0

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ $(function () {
     {
       boxClass: 'wow',
       animateClass: 'animate__animated --active',
-      offset: 300,
+      offset: 150,
       mobile: false,
     }
   )
@@ -12,14 +12,37 @@ $(function () {
   wow.init();
 
 
-  $("#menu").on("click", "a", function (event) {
+  $(".navbar__menu").on("click", "a", function (event) {
     var id = $(this).attr('href'),
       top = $(id).offset().top;
     $('body,html').animate({ scrollTop: top }, 1500);
+    $('.navbar__menu').toggleClass('navbar__menu--active');
+    $('.burger-btn').removeClass('burger-btn--active');
   });
 
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+  $('.testimonial-slider').slick({
+    arrows: false,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        },
+        breakpoint: 560,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ],
+  });
+
+  $('.burger-btn').on('click', function(){
+    $(this).toggleClass('burger-btn--active');
+    $('.navbar__menu').toggleClass('navbar__menu--active');
+  });
 
 });
